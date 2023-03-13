d81b3413b0026b72561192f74f015d4639215336
Ethan <et.l.nicholas@gmail.com>
2023-03-13 21:27:02 +1100

    add method for current block chain

--- blockchain.py
+++ blockchain.py
@@ -9,7 +9,12 @@ class Blockchain:
         genesis_block = self._create_block(data="This is a the first information to create genesis block", proof= 1 , previous_hash="0", index=1)
         self.chain.append(genesis_block)
 
-    def mine_block(sef, data:str) -> dict:
+    def curr_block(self, data:str) -> dict:
+        previous_block = self.get_previous_block()
+        previous_proof = previous_block["proof"]
+        previous_index = previous_block["index"]
+        index = len(self.chain) + 1
+        proof = None
         pass
 
     def get_previous_block(self) -> dict:
