abc6bc26df2736475878e2a8c00d145d53a20da1
Ethan <et.l.nicholas@gmail.com>
2023-03-13 21:24:14 +1100

    add methods get_previous_block and curr_block to the Blockchain class

--- blockchain.py
+++ blockchain.py
@@ -6,7 +6,15 @@ import json as _json
 class Blockchain:
     def __init__(self) -> None:
         self.chain= list()
-        genesis_block = None
+        genesis_block = self._create_block(data="This is a the first information to create genesis block", proof= 1 , previous_hash="0", index=1)
+        self.chain.append(genesis_block)
+
+    def mine_block(sef, data:str) -> dict:
+        pass
+
+    def get_previous_block(self) -> dict:
+        return self.chain[-1]
+         
     
     def _create_block(self, data: str, proof: int, previous_hash: str, index: int) -> dict:
         block = {
