78dc24776f4996e963204c261e15f331795e32ec
Ethan <et.l.nicholas@gmail.com>
2023-03-16 21:28:33 +1100

    add hash.256 to block

--- blockchain.py
+++ blockchain.py
@@ -10,13 +10,21 @@ class Blockchain:
             data="This is a the first information to create genesis block", proof=1, previous_hash="0", index=1)
         self.chain.append(genesis_block)
 
+    def _hash(self, block: dict) -> str:
+        encoded_block = _json.dumps(block, sort_keys= True).encode()
+        return _hashlib.sha256(encoded_block).hexdigest()
+
     def curr_block(self, data: str) -> dict:
         previous_block = self.get_previous_block()
         previous_proof = previous_block["proof"]
         previous_index = previous_block["index"]
         index = len(self.chain) + 1
         proof = self._proof_of_work(previous_proof,index,data)
-        pass
+        previous_hash = self._hash(block=previous_block)
+        block = self._create_block(data=data, proof=proof, previous_hash=previous_hash,index=index)
+
+        self.chain.append(block)
+        return block
 
     def _to_digest(self, new_proof: int, previous_proof: int, index: str, data: str) -> bytes:
         to_digest = str(new_proof**2 - previous_proof**2 + index) + data
@@ -26,8 +34,7 @@ class Blockchain:
         new_poof = 1
         check_proof = False
 
-        while not check_proof:
-            print(new_poof)
+        while not check_proof: 
             to_digest = self._to_digest(
                 new_proof=new_poof, previous_proof=previous_proof, index=index, data=data,)
             hash_value = _hashlib.sha256(to_digest).hexdigest()
